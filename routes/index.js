7613e5fe5fc39ba7f8b216858b7f5383e687d2a0
xavier_la <xavier.lagente@gmail.com>
2019-06-05 11:14:28 +0200

    Task 1 Create a GET /players endpoint returning all the players (using headtohead.json listed above).
    The list must be sorted by player id.

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,20 @@
 const express = require('express')
 const app = express()
+const request = require('request');
 
 app.get('/', (req, res) => {
     res.status(200).send('Hello World!')
+})
+
+app.get('/players', (req, res) => {
+  console.log(req.params.id)
+  request(`https://eurosportdigital.github.io/eurosport-node-developer-recruitment/headtohead.json`, function(error, response, body) {
+    body = JSON.parse(body);
+    res.json({ players: body.players.sort(function(a, b) {
+    return parseFloat(a.id) - parseFloat(b.id);
+      })
+    });
+  });
 })
 
 module.exports = app
