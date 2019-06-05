d5a57ffaddaaa241340ad2b335bbe875973c6d7b
xavier_la <xavier.lagente@gmail.com>
2019-06-05 11:16:09 +0200

    Task 2 Create a GET /players/<id> endpoint to return a single player matching id <id>.
    Return a 404 if no player is matching.

--- routes/index.js
+++ routes/index.js
@@ -17,4 +17,23 @@ app.get('/players', (req, res) => {
   });
 })
 
+app.get('/players/:id', (req, res) => {
+  request(`https://eurosportdigital.github.io/eurosport-node-developer-recruitment/headtohead.json`, function(error, response, body) {
+    body = JSON.parse(body);
+    find = -1
+    for (var i = 0; i < body.players.length; i++){
+  // look for the entry with a matching `code` value
+      if (body.players[i].id == req.params.id){
+          find = i
+        }
+      }
+    if(find > -1 ){
+      res.json({ player: body.players[find]})
+    }else{
+        res.status(404).send('player not Found')
+    }
+
+  });
+})
+
 module.exports = app
